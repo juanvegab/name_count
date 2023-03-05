3bbfa5a9ee5e30d633e44db3e356920a3505b630
Juan Carlos Vega Abarca <juanvegab23@gmail.com>
2023-03-04 23:12:19 -0600

    Welcome message added
    Signed-off-by: Juan Carlos Vega Abarca <juanvegab23@gmail.com>

--- lib/name_count_web/live/live_page.ex
+++ lib/name_count_web/live/live_page.ex
@@ -6,19 +6,19 @@ defmodule NameCountWeb.LivePage do
       NameCountWeb.Endpoint.subscribe(topic())
     end
 
-    list_of_names = NameCount.Counts.count_names!(5)
+    list_of_names = NameCount.Counts.count_names!(10)
 
-    {:ok, assign(socket, names: list_of_names)}
+    {:ok, assign(socket, names: list_of_names, sentName: nil)}
   end
 
-  def handle_info(%{event: "addName"}, socket) do
-    list_of_names = NameCount.Counts.count_names!(5)
-    {:noreply, assign(socket, names: list_of_names)}
+  def handle_info(%{event: "addName", payload: payload}, socket) do
+    list_of_names = NameCount.Counts.count_names!(10)
+    {:noreply, assign(socket, names: list_of_names, sentName: payload.name)}
   end
 
   def handle_event("send", %{"text" => text}, socket) do
     NameCount.Counts.create_name(%{name: text})
-    NameCountWeb.Endpoint.broadcast(topic(), "addName", %{})
+    NameCountWeb.Endpoint.broadcast(topic(), "addName", %{name: text})
 
     {:noreply, socket}
   end
@@ -35,6 +35,9 @@ defmodule NameCountWeb.LivePage do
         <input style="width: 200px" type="text" name="text" />
         <button type="submit">Send</button>
       </form>
+      <%= unless is_nil(@sentName) do %>
+        <p>Hello <%= @sentName%>! Welcome!!!</p>
+      <% end %>
       <div class="messages" style="border: 1px solid #eee; height: 400px; overflow: scroll; margin-bottom: 8px;">
         <%= for n <- @names do %>
           <p style="margin: 2px;"><%= n.name%>: <%= n.count %></p>
